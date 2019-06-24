b2107766803c062559268dd5d293f194a0685093
Outro Usuario <outrousuariogit@gmail.com>
2019-06-24 20:39:29 -0300

    teste 5

--- src/GitTutorial/Models/ErrorViewModel.cs
+++ src/GitTutorial/Models/ErrorViewModel.cs
@@ -2,6 +2,11 @@ namespace GitTutorial.Models
 {
     public class ErrorViewModel
     {
+        public ErrorViewModel()
+        {
+            Message = "OK";
+        }
+
         public string RequestId { get; set; }
         public string Message { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
