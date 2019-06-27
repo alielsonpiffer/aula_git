445cf0b29c3e23bedf9c76e8507d45c7bc4f3740
Outro Usuario <outrousuariogit@gmail.com>
2019-06-27 10:42:58 -0300

    teste 11

--- src/GitTutorial/Controllers/HomeController.cs
+++ src/GitTutorial/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GitTutorial.Models;
 using Microsoft.AspNetCore.Mvc;
-using GitTutorial.Models;
+using System.Diagnostics;
 
 namespace GitTutorial.Controllers
 {
@@ -18,6 +14,7 @@ namespace GitTutorial.Controllers
         public IActionResult Privacy()
         {
             return View();
+            //alteração
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
